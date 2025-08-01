c1a01e28225c0be2af73f90b5a254cfa48a955d6
banzarkhan <banzarkhanova93@gmail.com>
2025-08-01 22:20:26 +0800

    Initial Commit

---
+++ DailyQuiz/DailyQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyQuizApp.swift
+//  DailyQuiz
+//
+//  Created by Ariuna Banzarkhanova on 01/08/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
